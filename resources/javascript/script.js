7dd67358aab442d71017fab19e31b5bf95fa376e
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-09-01 21:43:38 +0300

    Add drawing function

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -12,10 +12,14 @@ function createSquare(id, squareClass){
     square.classList.add(squareClass);
     square.id = id;
     square.style.cssText =
-     `background-color: ${DEFAULT_COLOR}; height: ${SIZE / now_count}px; width: ${SIZE / now_count}px;`;
+        `background-color: ${DEFAULT_COLOR}; height: ${SIZE / now_count}px; width: ${SIZE / now_count}px;`;
+    square.addEventListener('mouseenter', (e) => changeColor(e, now_paint_color));
     return square;
 }
 
+function changeColor(e, color) {
+    console.log(e.target.style.backgroundColor = color);
+}
 
 const field = document.querySelector('.field');
 
