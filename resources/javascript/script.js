20182eab0f717cb5734be003dca990eedc1855af
greatest-frog <110398760+greatest-frog@users.noreply.github.com>
2022-09-01 22:58:40 +0300

    Add a scale for selecting the number of squares

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -7,23 +7,54 @@ let now_count = DEFAULT_COUNT;
 let now_color = DEFAULT_COLOR;
 let now_paint_color = DEFAULT_PAINT_COLOR;
 
+const field = document.querySelector('.field');
+
+const scale = document.querySelector('#pixel_count');
+const sizeDiv = document.querySelector('.sizeValue');
+
+scale.addEventListener('input', (e) => {
+    changeField(e);
+    changeSizeDiv();
+});
+
+// Creates a field with the actual number of squares
+function makeField() {
+    field.textContent = '';
+    for (let i = 0; i < now_count*now_count; i++) {
+        const square = createSquare((i+1).toString(), 'square');
+        field.appendChild(square);
+    }
+}
+
+// create one square
 function createSquare(id, squareClass){
     const square = document.createElement('div');
     square.classList.add(squareClass);
-    square.id = id;
+    square.id = `pixel-${id}`;
     square.style.cssText =
         `background-color: ${DEFAULT_COLOR}; height: ${SIZE / now_count}px; width: ${SIZE / now_count}px;`;
     square.addEventListener('mouseenter', (e) => changeColor(e, now_paint_color));
     return square;
 }
 
+// change the color of paint
 function changeColor(e, color) {
-    console.log(e.target.style.backgroundColor = color);
+    e.target.style.backgroundColor = color;
 }
 
-const field = document.querySelector('.field');
-
-for (let i = 0; i < 16*16; i++) {
-    const square = createSquare((i+1).toString(), 'square');
-    field.appendChild(square);
+// change the number of squares
+function changeCount(e) {
+    now_count = e.target.value;
 }
+
+// change the field
+function changeField(e) {
+    changeCount(e);
+    makeField();
+}
+
+function changeSizeDiv() {
+    sizeDiv.textContent = `${now_count} x ${now_count}`;
+}
+
+makeField();
